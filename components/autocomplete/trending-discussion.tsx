ade72c6b7b868e43d7db5da5a001f0f7e2d50f2e
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-06-02 21:01:02 +0600

    feat: get trending discussion from api

--- components/autocomplete/trending-discussion.tsx
+++ components/autocomplete/trending-discussion.tsx
@@ -1,6 +1,23 @@
+import { getPostsData } from "@/actions/getPosts";
+import { POSTS_PER_PAGE } from "@/config/constants";
+import { PostItem } from "@/types/post";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
 import SearchResultItem from "./search-result-item";
 
 const TrendingDiscussionSuggestion = () => {
+  const [trendingData, setTrendingData] = useState<PostItem[] | null>(null);
+  const [isLoading, setIsLoading] = useState(false)
+  const fetchTrendingData = async () => {
+    setIsLoading(true)
+    const data = await getPostsData(0, POSTS_PER_PAGE);
+    setTrendingData(data);
+    setIsLoading(false)
+  };
+  useEffect(() => {
+    fetchTrendingData();
+  }, []);
+
   return (
     <div className="w-full">
       <h3 className="uppercase font-medium text-xs text-foreground/80 px-3 mb-2">
@@ -8,16 +25,15 @@ const TrendingDiscussionSuggestion = () => {
       </h3>
 
       <div className="grid divide-y divide-border">
-        {Array.from(Array(8).keys()).map((item) => (
-          <SearchResultItem
-            key="item"
-            item={{
-              id: 1,
-              body: "some long body is here going to show so be palong body is here going to show so be pa long body is here going to show so be pa",
-              title: "Best title it is however you do not know!",
-            }}
-          />
-        ))}
+        {isLoading && (
+          <div className="flex items-end justify-center pb-4">
+            <Loader2 className="h-5 w-5 animate-spin text-center" />
+          </div>
+        )}
+        {!isLoading && trendingData &&
+          trendingData.map((item) => (
+            <SearchResultItem key={item.id} item={item} />
+          ))}
       </div>
     </div>
   );
